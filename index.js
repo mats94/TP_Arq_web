667ef3dcf34361a3e31ce738b6c4e8715c4134f3
matias <matias.antonio.barros@ibm.com>
2019-04-12 19:23:54 -0300

    mas endpoints

--- index.js
+++ index.js
@@ -2,11 +2,27 @@ var express = require('express');
 var app = express();
 const port = 3200;
 
-app.get('/', function (req, res) {
+app.listen(port, function () {
+    console.log('App listening on port ' + port);
+});
+
+app.get('/', function (req, res) { //homepage
     res.status(200);
     res.send('Hello World!');
 });
 
-app.listen(port, function () {
-    console.log('App listening on port ' + port);
+app.get('/envio/:Id', function (req, res) { //consulta de 1 envio especifico
+    res.status(200);
+    res.json({ "ENVIO" : { 'status' : 'demorado' , "ID" : req.params.Id , "Destino" : "Calle falsa 123" , "Remitente" : "Juan Perez"}})
+});
+
+app.post('/envio', function (req, res) { //crear envio
+    res.status(200);
+    //creacion del nuevo envio
+    res.send('Nuevo envio creado');
+});
+
+app.update('/envio/:Id', function (req, res) { //modificar envio
+    res.status(200);
+    res.json({ "ENVIO" : { 'status' : 'demorado' , "ID" : req.params.Id , "Destino" : "Calle falsa 123" , "Remitente" : "Juan Gonzalez"}})
 });
